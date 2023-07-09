cc94f54bcc5e07076796c0bff01a41eb21808dc1
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-07-09 12:31:51 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  namespace :public do
+    get 'homes/top'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+
+  get '/' => 'public/homes#top'
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20230709081150_create_order_details.rb
@@ -0,0 +1,11 @@
+class CreateOrderDetails < ActiveRecord::Migration[6.1]
+  def change
+    create_table :order_details do |t|
+      t.integer :item_id, null: false, foreign_key: true
+      t.integer :order_id, null: false, foreign_key: true
+      t.integer :price, null: false
+      t.integer :quantity, null: false
+      t.timestamps
+    end
+  end
+end

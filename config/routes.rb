e572ed2618785ec940a941a5af9b71e87aa8815f
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-07-29 12:35:00 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :costomers
-  scope module: :public do
-    root to: 'homes#top'
-    get 'homes/top'
-    get 'customers/mypage' => 'customers#show'
-    get 'customers/edit'
-    get 'customers/check'
-    patch 'customers/update'
-     # 退会確認画面
-    get  '/customers/check' => 'customers#check'
- # 論理削除用のルーティング
-    patch  '/customers/withdraw' => 'customers#withdraw'
-  end
-  namespace :admin do
-    get 'homes/top'
-
-  end
-
-  get '/' => 'public/homes#about'
-  # 顧客用
+   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -30,5 +11,21 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about'
+    resources :items, only: [:index, :show]
+    get 'customers/mypage' => 'customers#show'
+
+     # 退会確認画面
+    get  '/customers/check' => 'customers#check'
+ # 論理削除用のルーティング
+    patch  '/customers/withdraw' => 'customers#withdraw'
+  end
+
+  get '/admin' => 'admin/homes#top'
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
 end

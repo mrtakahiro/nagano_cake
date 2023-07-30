76b73eb81bfaff01fbbbe0ea1d4643bc6604688b
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-07-30 12:25:38 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -16,16 +16,23 @@ Rails.application.routes.draw do
     root to: 'homes#top'
     get '/about' => 'homes#about'
     resources :items, only: [:index, :show]
+    resources :customers, only: [:edit, :update]
     get 'customers/mypage' => 'customers#show'
-
      # 退会確認画面
     get  '/customers/check' => 'customers#check'
  # 論理削除用のルーティング
     patch  '/customers/withdraw' => 'customers#withdraw'
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    delete '/cart_items/destroy_all' => 'cart_items#destory_all'
+    resources :orders, only: [:new, :create, :index, :show]
+    post '/orders/check' => 'orders#check'
+    get '/orders/completion' => 'orders#completion'
   end
 
   get '/admin' => 'admin/homes#top'
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
   end
 end

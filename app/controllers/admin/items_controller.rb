6eb6a56f069f5526fc382b83af5d3d506515effc
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-08-01 12:26:28 +0000

    update

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -7,4 +7,15 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    item = Item.new(list_params)
+    item.save
+    redirect_to '/admin/items/[:id]'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price)
+  end
 end

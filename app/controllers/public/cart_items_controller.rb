1ec95d9c3f375d14c5f08f064bd3a3a6279307d8
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-08-16 12:33:16 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_items = current_customer.cart_items
+     @sum = 0
   end
 
   def update
@@ -17,8 +18,14 @@ class Public::CartItemsController < ApplicationController
 
   def create
     cart_item = CartItem.new(cart_item_params)
-    cart_item.customer_id = current_customer.id
-    cart_item.save
+    if current_customer.cart_items.find_by(item_id: cart_item.item_id) #CartItem.find_by(item_id: cart_item.item_id, customer_id: current_customer.id)
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+    else
+      cart_item.customer_id = current_customer.id
+      cart_item.save
+    end
     redirect_to cart_items_path
   end
 

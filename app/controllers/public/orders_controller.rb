08caf071fed7d31fb03f27258939d90d2115201f
mrtakahiro <128272534+mrtakahiro@users.noreply.github.com>
2023-08-20 13:18:16 +0000

    update

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def check
@@ -9,11 +10,19 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    redirect_to orders_check_path(post)
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def list_params
+    params.require(:ordef).permit(:customer_id, :name, :postal_code, :address)
   end
 end
